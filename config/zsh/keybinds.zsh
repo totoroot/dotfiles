f13d3f758234f545fe887a96d06d1acebf0c7753
Matthias Thym <git@thym.at>
2021-01-05 00:18:06 +0100

    Added substring search keybinds

--- config/zsh/keybinds.zsh
+++ config/zsh/keybinds.zsh
@@ -1,4 +1,9 @@
+# <Ctrl>/<Alt> + <Left>/<Right>
 bindkey "^[[1;3D" backward-word
 bindkey "^[[1;3C" forward-word
 bindkey "^[[1;5C" beginning-of-line
 bindkey "^[[1;5D" end-of-line
+
+# bind UP and DOWN arrow keys
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
