a87352baae42ec586c138049e55b46a2c2851930
Matthias Thym <git@thym.at>
2021-01-08 12:03:05 +0100

    Install sshpass

--- modules/services/ssh.nix
+++ modules/services/ssh.nix
@@ -1,4 +1,4 @@
-{ options, config, lib, ... }:
+{ options, config, pkgs, lib, ... }:
 
 with lib;
 with lib.my;
@@ -9,6 +9,10 @@ in {
   };
 
   config = mkIf cfg.enable {
+    user.packages = with pkgs; [
+      sshpass
+    ];
+    
     services.openssh = {
       enable = true;
       challengeResponseAuthentication = false;
@@ -16,7 +20,7 @@ in {
     };
 
     user.openssh.authorizedKeys.keys = [
-      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC8Tl2pj6CPBV3V72cohPhn+k0a/Na3cOrbmUtFC7rR7icWocSs1S1cSrP3sPLwUqRh3+zfIuLXTrTv3gj8Xvg8WELOspzsiYeAtbcHrnWnx/a7xzYvFyT9/8hkiGM/F6w7IuKEk+AZW34vARSgRPJ1FdH8NbPKJ8ay9zW9XB9YJGnbzIRmsVVpQ8l6Fh8ZqRjZfC1ea7hns8+HgjPrIHFb+S3qZZiwU4Gc8aWJy9ziwwkllEsSchv3aigYA3eOeW0FUQFiKsLGxbX2b2b3d6jFO4Pu+dMSen0h5IzBo0nh7UADSfJPdwbZaMuJzviKe2y6zg6jaM9XRIhLBT6bftDr henrik@lissner.net"
+      ""
     ];
   };
 }
