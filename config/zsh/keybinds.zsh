fd13d77b95d6f010f17c6b922559ccf0d826dd98
Matthias Thym <git@thym.at>
2021-01-08 20:07:54 +0100

    Tried fixing zsh keybinding and monitor issue

--- config/zsh/keybinds.zsh
+++ config/zsh/keybinds.zsh
@@ -4,20 +4,8 @@ bindkey '^[[1;3C' forward-word
 bindkey '^[[1;5D' beginning-of-line
 bindkey '^[[1;5C' end-of-line
 # bindkey '^[[3;3~' forward delete word
-bindkey '^[[3;5~' vi-kill-eol
+bindkey '^[[3;5~' kill-eol
 bindkey '^H' backward-kill-line
-
-# bind UP and DOWN arrow keys
-
-# TODO fix zsh-history-substring-search
-zle -N history-substring-search-up
-bindkey '^[[A' history-substring-search-up
-zle -N history-substring-search-down
-bindkey '^[[B' history-substring-search-down
-
-# fuzzy finder on <Ctrl> + <F>
-zle -N fzf
-bindkey '^F' fzf
 
 # C-z to toggle current process (background/foreground)
 fancy-ctrl-z () {
@@ -35,3 +23,15 @@ bindkey '^Z' fancy-ctrl-z
 # Open current prompt in external editor
 autoload -Uz edit-command-line; zle -N edit-command-line
 bindkey '^E' edit-command-line
+
+# bind UP and DOWN arrow keys
+
+# TODO fix zsh-history-substring-search
+# zle -N history-substring-search-up
+# bindkey '^[[A' history-substring-search-up
+# zle -N history-substring-search-down
+# bindkey '^[[B' history-substring-search-down
+
+# fuzzy finder on <Ctrl> + <F>
+# zle -N fzf
+# bindkey '^F' fzf
