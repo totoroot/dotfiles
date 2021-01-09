32c24cdbda78c0e69eca05966e556011623de0b4
Matthias Thym <git@thym.at>
2021-01-09 23:49:40 +0100

    Added jitsi-meet and delta-chat

--- modules/desktop/communication/element.nix
+++ modules/desktop/communication/element.nix
@@ -2,15 +2,15 @@
 
 with lib;
 with lib.my;
-let cfg = config.modules.desktop.communication.jitsi;
+let cfg = config.modules.desktop.communication.element;
 in {
-  options.modules.desktop.communication.jitsi = {
+  options.modules.desktop.communication.element = {
     enable = mkBoolOpt false;
   };
 
   config = mkIf cfg.enable {
     user.packages = with pkgs; [
-      jitsi-meet-electron
+      element-desktop
     ];
   };
 }
