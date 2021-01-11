3f3ca6b0d8ac50f3f3a7628272355f84e077a904
Matthias Thym <git@thym.at>
2021-01-11 20:44:47 +0100

    Moved evolution to separate module

--- modules/desktop/mail.nix
+++ modules/desktop/mail.nix
@@ -8,16 +8,13 @@ let cfg = config.modules.desktop.mail;
 in {
   options.modules.desktop.mail = {
     enable             = mkBoolOpt false;
-    aerc.enable        = mkBoolOpt true;
-    evolution.enable   = mkBoolOpt true;
   };
 
   config = mkIf cfg.enable {
     user.packages = with pkgs; [
       # install aerc version from nixpkgs (often not latest)
-      (mkIf cfg.aerc.enable unstable.aerc)
-      (mkIf cfg.aerc.enable w3m)
-      (mkIf cfg.evolution.enable gnome3.evolution)
+      unstable.aerc
+      w3m
     ];
 
     home.configFile = {
