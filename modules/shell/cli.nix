307efcaabccde4da66f33b6ab0ba8b8a5970fb99
Matthias Thym <git@thym.at>
2021-01-11 12:44:22 +0100

    Added json and yaml stuff

--- modules/shell/cli.nix
+++ modules/shell/cli.nix
@@ -11,32 +11,35 @@ in {
   config = mkIf cfg.enable {
     user.packages = with pkgs; [
       ## system utilities
-      gotop           # graphical activity monitor
+      gotop               # graphical activity monitor
       zenith          # zoomable system monitor
-      tree            # directory listing as tree
-      pstree   			  # show running processes as tree
-      sd 				      # intuitive sed alternative
-      fd 				      # intuitive find alertnative
-      nvtop				    # task monitor for Nvidia GPUs
-      zip				      # create/modify zip archives
-      unzip				    # extract zip archives
-      exiftool			  # metadata tool
-      aria2				    # multi-protocol download utility
-      borgbackup		  # backup utility with compression and encryption
-      playerctl       # control media players from command line
-      light           # control display brightness from command line
-      colorpicker     # X11 color picker
+      tree                # directory listing as tree
+      pstree   			      # show running processes as tree
+      sd 				          # intuitive sed alternative
+      unstable.fd         # intuitive find alertnative
+      nvtop				        # task monitor for Nvidia GPUs
+      zip				          # create/modify zip archives
+      unzip				        # extract zip archives
+      exiftool			      # metadata tool
+      aria2				        # multi-protocol download utility
+      unstable.borgbackup # backup utility with compression and encryption
+      playerctl           # control media players from command line
+      light               # control display brightness from command line
+      colorpicker         # X11 color picker
+      jq                  # lightweight JSON processor 
+      unstable.yq         # jq wrapper for YAML files
+      unstable.yamllint   # linter for yaml files
       ## file managers
-      lf              # terminal file manager
+      unstable.lf         # terminal file manager
       ## other fun stuff
-      gomatrix			  # matrix effect
-      lolcat			    # freakin rainbow terminal effect
-      nms 				    # data encryption effect
-      pdfcpu			    # cli pdf processor
-      pdfpc				    # pdf presenter console
-      neofetch			  # system info script
-      cheat				    # interactive cheatsheets
-      tokei				    # code statistics
+      gomatrix			      # matrix effect
+      lolcat			        # freakin rainbow terminal effect
+      nms 				        # data encryption effect
+      pdfcpu			        # cli pdf processor
+      pdfpc				        # pdf presenter console
+      neofetch			      # system info script
+      cheat				        # interactive cheatsheets
+      tokei				        # code statistics
     ];
 
     user.extraGroups = [ "admin" ];
