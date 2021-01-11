a475047da645f7dc859d4d67a7771963e2bf58dc
Matthias Thym <git@thym.at>
2021-01-11 20:45:29 +0100

    Added anki und gpg gui

---
+++ modules/desktop/apps/anki.nix
@@ -0,0 +1,18 @@
+# modules/desktop/apps/anki.nix
+
+{ config, options, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.desktop.apps.anki;
+in {
+  options.modules.desktop.apps.anki = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    user.packages = with pkgs; [
+      anki
+    ];
+  };
+}
