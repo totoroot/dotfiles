50125af6b4751c0daddbeadbef1ae44fe12ee4af
Matthias Thym <git@thym.at>
2021-01-16 16:07:08 +0100

    Removed celluloid package

--- modules/desktop/media/video.nix
+++ modules/desktop/media/video.nix
@@ -19,7 +19,6 @@ in {
       ffmpeg
       (mkIf cfg.player.enable mpv-with-scripts)
       (mkIf cfg.player.enable mpvc)
-      (mkIf cfg.player.enable celluloid)
       (mkIf cfg.player.enable vlc)
       (mkIf cfg.recording.enable obs-studio)
       (mkIf cfg.recording.enable vokoscreen)
