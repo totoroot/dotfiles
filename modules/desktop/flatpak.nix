dff64b8d61efda4addfe8488f5d66881346500e7
Matthias Thym <git@thym.at>
2021-01-16 16:56:56 +0100

    Moved image viewers and flatpak config from default.nix to suiting modules

---
+++ modules/desktop/flatpak.nix
@@ -0,0 +1,27 @@
+# modules/desktop/flatpak.nix
+
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.desktop.flatpak;
+in {
+  options.modules.desktop.flatpak = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      unstable.flatpak
+    ];
+
+    # Enable flatpak to install packages from flathub
+    services.flatpak.enable = true;
+    
+    # Enable portal for xdg files for packages installed with flatpak
+    xdg.portal = {
+      enable = true;
+      extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    };
+  };
+}
