09d1095a342a8f15812df53e19fdbef52f083a96
Matthias Thym <git@thym.at>
2021-01-27 22:56:10 +0100

    Last push before gone

--- modules/themes/alucard/config/polybar/run.sh
+++ modules/themes/alucard/config/polybar/run.sh
@@ -1,7 +1,9 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
 pkill -u $UID -x polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+export MONITORS=( $(xrandr -q | grep ' connected' | cut -d' ' -f1) )
 
 polybar main >$XDG_DATA_HOME/polybar.log 2>&1 &
 echo 'Polybar launched...'
