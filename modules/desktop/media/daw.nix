e42af831048aa05ef8b202536ac0070bff8fc3c7
Matthias Thym <git@thym.at>
2021-01-31 21:31:48 +0100

    Added lilypond music typesetting program

--- modules/desktop/media/daw.nix
+++ modules/desktop/media/daw.nix
@@ -17,11 +17,10 @@ in {
 
   config = mkIf cfg.enable {
     user.packages = with pkgs; [
-      # lmms on stable is broken due to 'Could not find the Qt platform plugin
-      # "xcb" in ""' error: https://github.com/NixOS/nixpkgs/issues/76074
-      lmms       # for making music
-      sunvox     # for making music (where LMMS is overkill)
-      audacity   # for recording and remastering audio
+      lmms        # for making music
+      sunvox      # for making music (where LMMS is overkill)
+      audacity    # for recording and remastering audio
+      lilypond    # music typesetting system
     ];
   };
 }
