6c430fb0b83238369208e87a672db57368630235
Matthias Thym <git@thym.at>
2021-02-19 20:14:36 +0100

    Remove reminder

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -62,8 +62,3 @@ take() {
 zman() {
   PAGER="less -g -I -s '+/^       "$1"'" man zshall;
 }
-
-r() {
-  local time=$1; shift
-  sched "$time" "notify-send --urgency=critical 'Reminder' '$@'; ding";
-}; compdef r=sched
