e4147168081cc114544ccf8870d63c3609998ad5
Matthias Thym <git@thym.at>
2021-03-28 15:01:30 +0200

    Delete old theme from hlissner

--- modules/themes/alucard/config/polybar/run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-
-pkill -u $UID -x polybar
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-export MONITORS=( $(xrandr -q | grep ' connected' | cut -d' ' -f1) )
-
-polybar main >$XDG_DATA_HOME/polybar.log 2>&1 &
-echo 'Polybar launched...'
