51821d190da9064e8fa9272399f2e0048cacdf90
Matthias Thym <git@thym.at>
2021-05-22 11:31:13 +0200

    Add Brother laserprinter

---
+++ modules/hardware/printers.nix
@@ -0,0 +1,17 @@
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.hardware.printers;
+in
+{
+  options.modules.hardware.printers = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    # Brother laser printer goes BRRRRRrrrrr
+    services.printing.enable = true;
+    services.printing.drivers = [ pkgs.brlaser ];
+  };
+}
