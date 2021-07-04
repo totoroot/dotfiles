36c140d148a9ef5c0832dcc453fcb14a3292fdaf
Matthias Thym <git@thym.at>
2021-07-04 17:46:19 +0200

    Set sudo command timeout to 60 minutes

--- modules/security.nix
+++ modules/security.nix
@@ -5,6 +5,11 @@
   boot.tmpOnTmpfs = true;
   security.protectKernelImage = true;
 
+  # Set sudo command timeout to 60 minutes
+  security.sudo.extraConfig = ''
+    Defaults        timestamp_timeout=60
+  '';
+
   # Fix a security hole in place for backwards compatibility. See desc in
   # nixpkgs/nixos/modules/system/boot/loader/systemd-boot/systemd-boot.nix
   boot.loader.systemd-boot.editor = false;
