07ce1afb20ea2c3ad73782d1e4b1c04a6d23f05c
Matthias Thym <git@thym.at>
2021-08-20 12:06:00 +0200

    Modify polybar config to include calendar and mullvad toggle

---
+++ modules/themes/quack/config/polybar/polybar-scripts/calendar.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+send_notification() {
+	TODAY=$(date '+%-d')
+	HEAD=$(cal -m "$1" | head -n1)
+	BODY=$(cal -m "$1" | tail -n7 | sed -z "s|$TODAY|<u><b>$TODAY</b></u>|1")
+	dunstify -h string:x-canonical-private-synchronous:calendar \
+		"$HEAD" "$BODY$FOOT" -u NORMAL
+}
+
+handle_action() {
+	echo "$DIFF" > "$TMP"
+	if [ "$DIFF" -ge 0 ]; then
+		send_notification "+$DIFF months"
+	else
+		send_notification "$((-DIFF)) months ago"
+	fi
+}
+
+TMP=${XDG_RUNTIME_DIR:-/tmp}/"$UID"_calendar_notification_month
+touch "$TMP"
+
+DIFF=$(<"$TMP")
+
+case $1 in
+	"curr") DIFF=0;;
+	"next") DIFF=$((DIFF+1));;
+	"prev") DIFF=$((DIFF-1));;
+esac
+
+handle_action
