d4a41198533c11fbc4e673f901e1a61f24389648
Matthias Thym <git@thym.at>
2021-12-12 15:57:05 +0100

    Reorganized Python config

--- modules/dev/python.nix
+++ modules/dev/python.nix
@@ -14,7 +14,8 @@ in {
     user.packages = with pkgs; [
       python38
       python38Packages.pip
-      python38Packages.pipdate    # pip update helpers
+      # pip update helpers
+      python38Packages.pipdate
       python38Packages.virtualenv
       python38Packages.pytest-virtualenv
       python38Packages.virtualenv-clone
@@ -22,16 +23,30 @@ in {
       python38Packages.ipython
       python38Packages.setuptools
       python38Packages.pylint
-      python38Packages.poetry     # Python dependency management and packaging made easy
-      python38Packages.jupyterlab # Jupyter lab environment notebook server extension
-      # python38Packages.jupytext   # Jupyter notebooks as Markdown documents, Julia, Python or R scripts
-      ansible                     # automation tool
-      ansible-lint                # linter for ansible
-      yamllint                    # linter for yaml files
-      esptool                     # ESP8266 and ESP32 serial bootloader utility
-      micropython                 # Python implementation for MCUs
-      pandoc                      # Conversion between markup formats
-      thonny                      # Python IDE for beginners
+      # Python dependency management and packaging made easy
+      python38Packages.poetry
+      # Jupyter lab environment notebook server extension
+      python38Packages.jupyterlab
+      # # Jupyter notebooks as Markdown documents, Julia, Python or R scripts
+      # python38Packages.jupytext
+      # # Scientific tools for Python
+      # python38Packages.numpy
+      # # Python Data Analysis Library
+      # python38Packages.pandas
+      # Automation tool
+      ansible
+      # Linter for ansible
+      ansible-lint
+      # Linter for yaml files
+      yamllint
+      # ESP8266 and ESP32 serial bootloader utility
+      esptool
+      # Python implementation for MCUs
+      micropython
+      # Conversion between markup formats
+      pandoc
+      # Python IDE for beginners
+      thonny
     ];
 
     env.IPYTHONDIR      = "$XDG_CONFIG_HOME/ipython";
