54047d360d3a3d860094dedefea5dcb81e5093d9
Matthias Thym <git@thym.at>
2021-12-17 21:53:41 +0100

    Create home directories on purple

--- hosts/purple/default.nix
+++ hosts/purple/default.nix
@@ -136,5 +136,25 @@
   # Set default monitor
   environment.variables = rec {
     MONITORS = ["HDMI-0" "DP-0"];
-  }; 
+  };
+
+  # Create some home directories
+  home.file = {
+    "archive/.use".text = "archive";
+    "books/.use".text = "books";
+    "dev/.use".text = "dev";
+    "documents/.use".text = "documents";
+    "downloads/.use".text = "downloads";
+    "graphics/.use".text = "graphics";
+    "inbox/.use".text = "inbox";
+    "music/.use".text = "music";
+    "notes/.use".text = "notes";
+    "outbox/.use".text = "outbox";
+    "photos/.use".text = "photos";
+    "resources/.use".text = "resources";
+    "shared/.use".text = "shared";
+    "tmp/.use".text = "tmp";
+    "uni/.use".text = "uni";
+    "zero/.use".text = "zero";
+  };
 }
