03740ece1396e7177f4220226293632dc1aebede
Matthias Thym <git@thym.at>
2022-01-12 01:47:38 +0100

    Add new headless server

---
+++ hosts/lilac/default.nix
@@ -0,0 +1,93 @@
+{ ... }:
+{
+  imports = [
+    ../personal.nix
+    ./hardware-configuration.nix
+  ];
+
+  ## Modules
+  modules = {
+    desktop = false;
+    dev = false;
+    editors = {
+      default = "micro";
+      vim.enable = true;
+      micro.enable = true;
+    };
+    hardware = {
+      audio.enable = true;
+      bluetooth.enable = true;
+      disks.enable = true;
+      fancontrol.enable = true;
+      keebs.enable = false;
+      nvidia.enable = false;
+      printers.enable = true;
+      sensors.enable = true;
+      wacom.enable = false;
+    };
+    shell = {
+      archive.enable = false;
+      borg.enable = false;
+      clipboard.enable = false;
+      direnv.enable = false;
+      git.enable = true;
+      gnupg.enable = true;
+      lf.enable = true;
+      mail.enable = false;
+      pass.enable = false;
+      taskell.enable  = false;
+      zsh.enable = true;
+      cli.enable = true;
+    };
+    services = {
+      docker.enable = true;
+      gitea.enable = false;
+      jellyfin.enable = false;
+      k8s.enable = true;
+      nginx.enable  = false;
+      vpn.enable = true;
+      ssh.enable = true;
+      transmission.enable	= false;
+    };
+    # theme.active = "quack";
+  };
+
+  ## Local config
+  programs.ssh.startAgent = true;
+  services.openssh.startWhenNeeded = true;
+  networking.networkmanager.enable = true;
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false
+  # here. Per-interface useDHCP will be mandatory in the future, so this
+  # generated config replicates the default behaviour.
+  networking.useDHCP = false;
+
+  # Set eurkey as default layout
+  # Optionally set more keymaps and use them with bin/keymapswitcher
+  services.xserver.layout = "eu";
+
+  # Set default monitor
+  # environment.variables = rec {
+    # MONITORS = ["HDMI-0" "DP-0"];
+  # };
+
+  # Create some home directories
+  home.file = {
+    "archive/.use".text = "archive";
+    "books/.use".text = "books";
+    "dev/.use".text = "dev";
+    "documents/.use".text = "documents";
+    "downloads/.use".text = "downloads";
+    "graphics/.use".text = "graphics";
+    "inbox/.use".text = "inbox";
+    "music/.use".text = "music";
+    "notes/.use".text = "notes";
+    "outbox/.use".text = "outbox";
+    "photos/.use".text = "photos";
+    "resources/.use".text = "resources";
+    "shared/.use".text = "shared";
+    "tmp/.use".text = "tmp";
+    "uni/.use".text = "uni";
+    "zero/.use".text = "zero";
+  };
+}
---
+++ hosts/lilac/hardware-configuration.nix
@@ -0,0 +1,60 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  imports =
+    [ (modulesPath + "/installer/scan/not-detected.nix")
+    ];
+
+  boot = {
+    initrd.availableKernelModules = [ "ahci" "ohci_pci" "ehci_pci" "xhci_pci" "usbhid" "uas" "sd_mod" ];
+    initrd.kernelModules = [];
+    extraModulePackages = [];
+    kernelModules = [
+      "kvm-amd"
+      "coretemp"
+      "it87"
+      "v4l2loopback"
+      "rtl8812au"
+      "k10temp"
+      "fam15h_power"
+    ];
+  };
+
+  # CPU
+  nix.maxJobs = lib.mkDefault 16;
+  powerManagement.cpuFreqGovernor = "performance";
+  hardware.cpu.amd.updateMicrocode = true;
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-label/nixos";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-label/boot";
+      fsType = "vfat";
+    };
+
+  fileSystems."/home" =
+    { device = "/dev/disk/by-label/home";
+      fsType = "ext4";
+      options = [ "noatime" ];
+    };
+
+  fileSystems."/mnt/data" =
+    { device = "/dev/disk/by-label/data";
+      fsType = "ext4";
+      options = [ "noatime" ];
+    };
+
+  fileSystems."/mnt/photos" =
+    { device = "/dev/disk/by-label/photos";
+      fsType = "ext4";
+      options = [ "noatime" ];
+    };
+
+  swapDevices = [];
+
+  # high-resolution display
+  hardware.video.hidpi.enable = lib.mkDefault true;
+}
