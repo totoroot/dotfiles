e3f859d181482a38931627a3754e0fa6fa33a98c
Matthias Thym <git@thym.at>
2022-01-12 04:03:38 +0100

    Add SSH pubkeys for private machines

--- modules/services/ssh.nix
+++ modules/services/ssh.nix
@@ -12,15 +12,21 @@ in {
     user.packages = with pkgs; [
       sshpass
     ];
-    
+
     services.openssh = {
       enable = true;
       challengeResponseAuthentication = false;
       passwordAuthentication = false;
     };
 
-    user.openssh.authorizedKeys.keys = [
-      ""
-    ];
+    user.openssh.authorizedKeys.keys =
+      if config.user.name == "mathym"
+      then [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM6pxS+faVh8CTTHw2ZZwnm9s54xNpDC6RJzxg43452g mathym@purple"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILplKT9yCU7in8VjPsxtxLZrhU8PajUJZascd0J4ILGv mathym@violet"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJe9HuM+MU/iO4QWVVmkTEZ79ybiKV1eCWXKx9wU8VKY mathym@lilac"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK7TpDoH2+3kWlkU8Zjbdfqcs/Qrw+H00Oc/ElLyB81M matthias@notebook"
+      ]
+      else [];
   };
 }
