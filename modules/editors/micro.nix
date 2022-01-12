d667544507ab2f23c4da16581a5cbee7346f5b17
Matthias Thym <git@thym.at>
2022-01-12 03:31:27 +0100

    Add lua init script for micro editor

--- modules/editors/micro.nix
+++ modules/editors/micro.nix
@@ -19,6 +19,7 @@ in {
     home.configFile = {
       "micro/settings.json".source = "${configDir}/micro/settings.json";
       "micro/bindings.json".source = "${configDir}/micro/bindings.json";
+      "micro/init.lua".source = "${configDir}/micro/init.lua";
       "micro/syntax".source = "${configDir}/micro/syntax";
       "micro/colorschemes".source = "${configDir}/micro/colorschemes/";
       "micro/plug".source = "${configDir}/micro/plug/";
