3370a148627da02f538ba5f85714d6c16b60611d
Matthias Thym <git@thym.at>
2022-02-02 18:14:39 +0100

    Add udev rules to set necessary permissions on input devices - Steam controller (wired + wireless) - Sony controllers (wired + wireless)

--- modules/hardware/steamcon.nix
+++ modules/hardware/steamcon.nix
@@ -17,7 +17,20 @@ in {
     # See https://wiki.archlinux.org/title/Gamepad#Steam_Controller_not_pairing for more info
     services.udev = {
       extraRules = ''
+        # This file allows SC-Controller application and daemon to access Steam Controller or its USB dongle.
+        # This is done by allowing read/write access to all users. You may want to change this to something like
+        # MODE="0660", GROUP="games" to allow r/w access only to members of that group.
+
+        # Valve USB devices
         SUBSYSTEM=="usb", ATTRS{idVendor}=="28de", MODE="0666"
+        # Valve HID devices over bluetooth hidraw
+        KERNEL=="hidraw*", KERNELS=="*28DE:*", MODE="0666", TAG+="uaccess"
+        # Sony USB devices
+        SUBSYSTEM=="usb", ATTRS{idVendor}=="054c", MODE="0666"
+        # Sony input devices over bluetooth
+        SUBSYSTEM=="input", KERNELS=="*054C:09CC*", MODE="0666", TAG+="uaccess"
+        # uinput kernel module write access (allows keyboard, mouse and gamepad emulation)
+        KERNEL=="uinput", SUBSYSTEM=="misc", TAG+="uaccess", OPTIONS+="static_node=uinput", MODE="0666"
         '';
     };
 
