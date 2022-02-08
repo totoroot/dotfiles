ca1bdac462998b737be6b8fa259f93d78a98f64b
Matthias Thym <git@thym.at>
2022-02-08 12:43:48 +0100

    Fix deprecated Bluetooth config and install packages

--- modules/hardware/bluetooth.nix
+++ modules/hardware/bluetooth.nix
@@ -7,32 +7,34 @@ let hwCfg = config.modules.hardware;
 in {
   options.modules.hardware.bluetooth = {
     enable = mkBoolOpt false;
-    audio.enable = mkBoolOpt false;
+    audio.enable = mkBoolOpt true;
   };
 
   config = mkIf cfg.enable (mkMerge [
     { hardware.bluetooth.enable = true; }
 
     (mkIf cfg.audio.enable {
+      environment.systemPackages = with pkgs; [
+        pulseaudioFull
+        bluez
+        bluez-tools
+        blueman
+      ];
+
       hardware.pulseaudio = {
         # NixOS allows either a lightweight build (default) or full build of
         # PulseAudio to be installed.  Only the full build has Bluetooth
         # support, so it must be selected here.
         # Also install bluetooth cli tools to make it usable.
-        package = with pkgs; [
-          pulseaudioFull
-          bluez
-          bluez-tools
-          blueman
-        ];
         # Enable additional codecs
         extraModules = [ pkgs.pulseaudio-modules-bt ];
       };
 
-      hardware.bluetooth.extraConfig = ''
-        [General]
-        Enable=Source,Sink,Media,Socket
-      '';
+      hardware.bluetooth.settings = {
+        General = {
+          Enable = "Source,Sink,Media,Socket";
+        };
+      };
     })
   ]);
 }
