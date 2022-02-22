6a4d4fbc336cff49f9296041f2286fa6c0bda39d
Matthias Thym <git@thym.at>
2022-02-22 13:41:35 +0100

    Change lf terminal file manager behaviour

--- modules/shell/lf.nix
+++ modules/shell/lf.nix
@@ -12,21 +12,38 @@ in {
 
   config = mkIf cfg.enable {
     user.packages = with pkgs; [
+      # Terminal file manager written in Go and heavily inspired by ranger
       lf
+      # cat(1) clone with syntax highlighting and Git integration
       bat
-      unrar
-      poppler
-      unzip
-      imagemagick
+      # MS-Word/Excel/PowerPoint to text converter
       catdoc
-      ffmpegthumbnailer
-      chafa
-      odt2txt
+      # Extracts plain text from docx files
+      catdocx
+      # Tool to read, write and edit EXIF meta information
       exiftool
-      transmission
+      # Lightweight video thumbnailer
+      ffmpegthumbnailer
+      # GNOME Office Spreadsheet
       gnumeric
+      # Software suite to create, edit, compose, or convert bitmap images
+      imagemagick
+      # Simple .odt to .txt converter
+      odt2txt
+      # Graphical pdf (and epub, cbz, ...) reader that works inside the kitty terminal
+      termpdfpy
+      # Fast, easy and free BitTorrent client
+      transmission
+      # Utility for RAR archives
+      unrar
+      # Extraction utility for archives compressed in .zip format
+      unzip
+      # # Terminal graphics for the 21st century !!! replaced by kitty's icat !!!
+      # chafa
+      # # PDF rendering library !!! replaced by termpdfpy !!!
+      # poppler
     ];
-      
+
     home.configFile = {
       "lf/lfrc".source = "${configDir}/lf/lfrc";
     };
