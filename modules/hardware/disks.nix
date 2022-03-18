ea4520d669310206989e1cc448ca65f27caceaa2
Matthias Thym <git@thym.at>
2022-03-18 16:08:03 +0100

    Rework disks module

--- modules/hardware/disks.nix
+++ modules/hardware/disks.nix
@@ -18,55 +18,48 @@ in {
       programs.udevil.enable = true;
 
       environment.systemPackages = with pkgs; [
-        # mount remote directories
-        sshfs
-        # mount exfat drives (macOS compatibility)
+        # Mount exfat drives (macOS compatibility)
         exfat
-        # utilities for the btrfs filesystem
+        # Utilities for the btrfs filesystem
         btrfs-progs
-        # creates and maintains the history of snapshots of btrfs filesystems
+        # Creates and maintains the history of snapshots of btrfs filesystems
         unstable.btrfs-snap
-        # visualize the layout of a mounted btrfs
+        # Visualize the layout of a mounted btrfs
         # btrfs-heatmap
-        # mount ntfs drives (Windows compatibility)
+        # Mount ntfs drives (Windows compatibility)
         ntfs3g
-        # non-destructive FAT16/FAT32 resizer
+        # Non-destructive FAT16/FAT32 resizer
         fatresize
-        # partitioning tool
+        # Partitioning tool
         parted
-        # graphical disk usage utility
+        # Graphical disk usage utility
         unstable.duf
-        # drive health monitoring
+        # Drive health monitoring
         smartmontools
-        # tool to get/set ATA/SATA drive parameters under Linux
+        # Tool to get/set ATA/SATA drive parameters under Linux
         hdparm
-        # get disk temperature for ATA/SATA drives
+        # Get disk temperature for ATA/SATA drives
         hddtemp
-        # menu-driven mounting tool for removeable drives
+        # Menu-driven mounting tool for removeable drives
         bashmount
-        # data recovery utilities
+        # Data recovery utilities
         testdisk
-        # tools to support Logical Volume Management (LVM)
+        # Tools to support Logical Volume Management (LVM)
         lvm2
         # NVM-Express user space tooling for Linux
         nvme-cli
         # LUKS disk encryption
         cryptsetup
-        # removable disk automounter for udisks
+        # Removable disk automounter for udisks
         udiskie
+        # Sync files and directories to and from major cloud storage
+        rclone
+        # Command-line WebDAV client
+        cadaver
       ];
 
-      # add user to group disk for access on disks without sudo
+      # Add user to group disk for access on disks without sudo
       user.extraGroups = [ "disk" ];
-
-      services.udev = {
-      extraRules = ''
-        KERNEL=="sd*", ACTION=="add", ATTR{removable}=="1", \
-        RUN+="$XDG_BIN_HOME/polybar-scripts/usb-mount.sh --update"
-        KERNEL=="sd*", ACTION=="remove", \
-        RUN+="$XDG_BIN_HOME/polybar-scripts/usb-mount.sh --update"
-        '';
-    };
     }
   ]);
 }
