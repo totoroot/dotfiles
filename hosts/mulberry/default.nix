e9d2e9838dbccf1a51127f36549e85443f0d61e8
Matthias Thym <git@thym.at>
2022-03-18 16:08:27 +0100

    Move home directories config to separate config

--- hosts/mulberry/default.nix
+++ hosts/mulberry/default.nix
@@ -3,6 +3,7 @@
   imports = [
     ../personal.nix
     ./hardware-configuration.nix
+    ./home.nix
   ];
 
   ## Modules
@@ -151,27 +152,5 @@
   # Set default monitor
   environment.variables = rec {
     MAIN_MONITOR = "eDP-1";
-  };
-
-  # Create some home directories
-  home.file = {
-    "archive/.use".text = "archive";
-    "books/.use".text = "books";
-    "dev/.use".text = "dev";
-    "documents/.use".text = "documents";
-    "downloads/.use".text = "downloads";
-    "games/.use".text = "games";
-    "graphics/.use".text = "graphics";
-    "inbox/.use".text = "inbox";
-    "music/.use".text = "music";
-    "notes/.use".text = "notes";
-    "outbox/.use".text = "outbox";
-    "photos/.use".text = "photos";
-    "resources/.use".text = "resources";
-    "shared/.use".text = "shared";
-    "tmp/.use".text = "tmp";
-    "uni/.use".text = "uni";
-    "zero/.use".text = "zero";
-    ".trash/.use".text = "trash";
   };
 }
---
+++ hosts/violet/home.nix
@@ -0,0 +1,23 @@
+{ ... }:
+{
+  home.file = {
+    "archive/.use".text = "archive";
+    "books/.use".text = "books";
+    "dev/.use".text = "dev";
+    "documents/.use".text = "documents";
+    "downloads/.use".text = "downloads";
+    "games/.use".text = "games";
+    "graphics/.use".text = "graphics";
+    "inbox/.use".text = "inbox";
+    "music/.use".text = "music";
+    "notes/.use".text = "notes";
+    "outbox/.use".text = "outbox";
+    "photos/.use".text = "photos";
+    "resources/.use".text = "resources";
+    "shared/.use".text = "shared";
+    "tmp/.use".text = "tmp";
+    "uni/.use".text = "uni";
+    "zero/.use".text = "zero";
+    ".trash/.use".text = "trash";
+  };
+}
