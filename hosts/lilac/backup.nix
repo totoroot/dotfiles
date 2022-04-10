042602cfe8910185dbfd50205447e57788fa6326
Matthias Thym <git@thym.at>
2022-04-10 21:08:58 +0200

    Create initial Syncthing config on lilac

--- hosts/lilac/backup.nix
+++ hosts/lilac/backup.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 {
   # This is needed to allow GUI access from remote hosts
   # More information on Syncthing's firewall config can be found here:
@@ -14,4 +14,30 @@
   # # This would expose port 8384 to everyone
   # networking.firewall.allowedTCPPorts = [ 8384 ];
   # networking.firewall.allowedUDPPorts = [ 8384 ];
+
+  user.packages = with pkgs [
+    # Open Source Continuous File Synchronization
+    syncthing
+  ];
+
+  services.syncthing = {
+    # Overrides any devices added or deleted through the WebUI
+    overrideDevices = false;
+    # Overrides any folders added or deleted through the WebUI
+    overrideFolders = false;
+
+    devices = {
+      purple.id = "C6E5H63-KPO3NWZ-WMIY4TN-PHQGLXQ-ZLQC2WL-YOX3BG2-BIFJ6RV-O2QKWAU";
+      violet.id = "ESP4NJ3-KP3QB7K-TN6G6S2-7PLQNB5-7CFQGVQ-DRICFJ2-PHGKTAO-GDHOGAR";
+      lilac.id = "4K4Q7HM-KLYVORI-QPVOZKJ-7Y27FSE-DU4M4D6-5PU6HAB-ZLHJJK4-6AQVNAS";
+      phone.id = "CHUU3Y7-XQ2MHGU-XYLHRQW-CZ6QW6C-WAG6R4A-XDW2Z3W-XBT7BLK-GE25KQO";
+    };
+
+    folders = {
+      "phone-backups" = {
+        path = "/mnt/data/backups/phone";
+        devices = "phone";
+      };
+    };
+  };
 }
