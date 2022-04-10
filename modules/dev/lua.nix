b06e74b654d2bdecc850a2b2e8aa4d18965563ac
Matthias Thym <git@thym.at>
2022-04-10 21:36:11 +0200

    Remove moonscript from Lua config

--- modules/dev/lua.nix
+++ modules/dev/lua.nix
@@ -1,9 +1,4 @@
-# modules/dev/lua.nix --- https://www.lua.org/
-#
-# I use lua for modding, awesomewm or Love2D for rapid gamedev prototyping (when
-# godot is overkill and I have the luxury of avoiding JS). I write my Love games
-# in moonscript to get around lua's idiosynchrosies. That said, I install love2d
-# on a per-project basis.
+# modules/dev/lua.nix
 
 { config, options, lib, pkgs, ... }:
 
@@ -13,14 +8,12 @@ let cfg = config.modules.dev.lua;
 in {
   options.modules.dev.lua = {
     enable = mkBoolOpt false;
-    love2D.enable = mkBoolOpt false;
   };
 
   config = mkIf cfg.enable {
     user.packages = with pkgs; [
+      # Powerful, fast, lightweight, embeddable scripting language
       lua
-      luaPackages.moonscript
-      (mkIf cfg.love2D.enable love2d)
     ];
   };
 }
