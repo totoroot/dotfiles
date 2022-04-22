65d23fb76bf551fe0b580a318e33efa1f762a9bf
Matthias Thym <git@thym.at>
2022-04-22 11:32:56 +0200

    Add fzf default options to zsh config

--- modules/shell/zsh.nix
+++ modules/shell/zsh.nix
@@ -64,6 +64,7 @@ in {
       ZSH_CACHE   = "$XDG_CACHE_HOME/zsh";
       ZGEN_DIR    = "$XDG_DATA_HOME/zsh";
       ZGEN_SOURCE = "$ZGEN_DIR/zgen.zsh";
+      FZF_DEFAULT_OPTS = "--reverse --ansi --inline-info --color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4";
     };
 
     home.configFile = {
