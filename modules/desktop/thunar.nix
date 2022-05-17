013b8878e1907016c3a90875f2761ccc8b1d3d09
Matthias Thym <git@thym.at>
2022-05-17 22:32:27 +0200

    Rework file

--- modules/desktop/thunar.nix
+++ modules/desktop/thunar.nix
@@ -1,20 +1,27 @@
-# modules/desktop/thunar.nix
+# modules/desktop/fm.nix
 
 { config, options, lib, pkgs, ... }:
 
 with lib;
 with lib.my;
-let cfg = config.modules.desktop.thunar;
+let cfg = config.modules.desktop.fm;
 in {
-  options.modules.desktop.thunar = {
+  options.modules.desktop.fm = {
     enable = mkBoolOpt false;
   };
 
   config = mkIf cfg.enable {
     user.packages = with pkgs; [
-      xfce.thunar
-      xfce.thunar-volman
+      # File manager with GTK interface
+      pcmanfm
+      # Xfce file manager
+      unstable.xfce.thunar
+      # Thunar extension for automatic management of removable drives and media
+      unstable.xfce.thunar-volman
+      # Thunar plugin providing file context menus for archives
       xfce.thunar-archive-plugin
+      # Thunar plugin providing tagging and renaming features for media files
+      xfce.thunar-media-tags-plugin
       # Virtual filesystem support library
       gvfs
     ];
