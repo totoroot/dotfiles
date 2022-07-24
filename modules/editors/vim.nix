b816a8eeb74c5a03c95aaf510c679613628c9da8
Matthias Thym <git@thym.at>
2022-07-24 15:28:36 +0200

    Install neovide as VIM GUI

--- modules/editors/vim.nix
+++ modules/editors/vim.nix
@@ -1,6 +1,3 @@
-# When I'm stuck in the terminal or don't have access to Emacs, (neo)vim is my
-# go-to. I am a vimmer at heart, after all.
-
 { config, options, lib, pkgs, ... }:
 
 with lib;
@@ -9,13 +6,19 @@ let cfg = config.modules.editors.vim;
 in {
   options.modules.editors.vim = {
     enable = mkBoolOpt false;
+    desktop.enable  = mkBoolOpt true;
   };
 
   config = mkIf cfg.enable {
-    user.packages = with pkgs; [
-      editorconfig-core-c
-      neovim
-    ];
+    user.packages = with pkgs;
+      (if cfg.enable then [
+        editorconfig-core-c
+        unstable.neovim
+      ] else []) ++
+
+      (if cfg.desktop.enable then [
+        unstable.neovide
+      ] else []);
 
     # This is for non-neovim, so it loads my nvim config
     # env.VIMINIT = "let \\$MYVIMRC='\\$XDG_CONFIG_HOME/nvim/init.vim' | source \\$MYVIMRC";
