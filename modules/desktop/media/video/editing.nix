e1d9fde0115c9ae93dd67b4373ba873856df6495
Matthias Thym <git@thym.at>
2022-08-29 19:00:18 +0200

    Add DaVinci Resolve to editing tools

--- modules/desktop/media/video/editing.nix
+++ modules/desktop/media/video/editing.nix
@@ -14,6 +14,8 @@ in {
     user.packages = with pkgs; [
       # Video editor
       kdenlive
+      # Professional Video Editing, Color, Effects and Audio Post
+      unstable.davinci-resolve
       # Node-graph based, open-source compositing software
       # FIXME Temporarily installed with flatpak due to broken Nix package
       # natron
