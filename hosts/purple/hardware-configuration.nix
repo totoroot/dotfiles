1c25f66155cc6332900b079c5faba2706c0aa054
Matthias Thym <git@thym.at>
2022-09-12 13:12:01 +0200

    Reformat disk confing and change mounted disks

--- hosts/purple/hardware-configuration.nix
+++ hosts/purple/hardware-configuration.nix
@@ -27,33 +27,33 @@
   powerManagement.cpuFreqGovernor = "performance";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/8c3e271d-84ae-4e4c-b30a-da4a922babf6";
-      fsType = "ext4";
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/8c3e271d-84ae-4e4c-b30a-da4a922babf6";
+    fsType = "ext4";
+  };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/6EAA-309D";
-      fsType = "vfat";
-    };
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/6EAA-309D";
+    fsType = "vfat";
+  };
 
-  fileSystems."/home" =
-    { device = "/dev/disk/by-uuid/942e62f5-549c-46f8-8b14-5ee4013de3e5";
-      fsType = "ext4";
-      options = [ "noatime" ];
-    };
+  fileSystems."/home" = {
+    device = "/dev/disk/by-uuid/5dfc5e4e-943f-4d31-8e9c-d4f6be6e2c5c";
+    fsType = "ext4";
+    options = [ "noatime" ];
+  };
 
-  fileSystems."/mnt/data" =
-    { device = "/dev/disk/by-uuid/ecea4b71-98e6-4c2a-befb-74bb4f35e39e";
-      fsType = "ext4";
-      options = [ "noatime" ];
-    };
+  fileSystems."/mnt/backup" = {
+    device = "/dev/disk/by-uuid/7e000029-709f-4098-a72d-7faab49ca2db";
+    fsType = "ext4";
+    options = [ "noatime" ];
+  };
 
-  fileSystems."/mnt/photos" =
-    { device = "/dev/disk/by-uuid/0ee239aa-88cd-4a40-a697-19beef515c0f";
-      fsType = "ext4";
-      options = [ "noatime" ];
-    };
+  fileSystems."/mnt/photos" = {
+    device = "/dev/disk/by-uuid/0ee239aa-88cd-4a40-a697-19beef515c0f";
+    fsType = "ext4";
+    options = [ "noatime" ];
+  };
 
   swapDevices = [ { device = "/dev/disk/by-uuid/81651822-eb2c-4670-9b64-263386393b9a"; } ];
 
