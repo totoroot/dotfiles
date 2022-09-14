8fc91de14e23bb806c33b7d3d96bc97271715d16
Matthias Thym <git@thym.at>
2022-09-14 19:22:57 +0200

    Add mpv config to make sure hardware-accelerated video playback is enabled

--- modules/desktop/media/video/player.nix
+++ modules/desktop/media/video/player.nix
@@ -12,10 +12,19 @@ in {
 
   config = mkIf cfg.enable {
     user.packages = with pkgs; [
-      ffmpeg-full
-      mpv-with-scripts
+      # A complete, cross-platform solution to record, convert and stream audio and video
+      ffmpeg_5
+      # General-purpose media player, fork of MPlayer and mplayer2
+      mpv
+      # A mpc-like control interface for mpv
       mpvc
+      # Cross-platform media player and streaming server
       vlc
     ];
+
+    
+    home.configFile = {
+      "mpv/mpv.conf".source = "${configDir}/mpv/mpv.conf";
+    };
   };
 }
