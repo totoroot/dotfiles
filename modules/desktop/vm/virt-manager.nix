213968bfd142025b7b8cfecd9e155ee818fd70de
Matthias Thym <git@thym.at>
2022-10-26 23:43:57 +0200

    Fix extraGroups for virt-manager and add comments

--- modules/desktop/vm/virt-manager.nix
+++ modules/desktop/vm/virt-manager.nix
@@ -12,11 +12,13 @@ in {
 
   config = mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
+      # Desktop user interface for managing virtual machines
       virt-manager
+      # A toolkit to interact with the virtualization capabilities of recent versions of Linux and other OSes
       libvirt
     ];
 
-    user.extraGroups = [ "libvirt" ];
+    user.extraGroups = [ "libvirtd" ];
 
     virtualisation.libvirtd.enable = true;
   };
