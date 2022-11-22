04d9d68ecbb686ed06447d1fd5e544d0ed535d93
Matthias Thym <git@thym.at>
2022-11-22 12:48:20 +0100

    Reformat violet hardware config and add settings

--- hosts/violet/hardware-configuration.nix
+++ hosts/violet/hardware-configuration.nix
@@ -1,23 +1,33 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "ehci_pci" "nvme" "usb_storage" "usbhid" "uas" "sd_mod" ];
-  boot.initrd.kernelModules = [ "dm-snapshot" ];
-  boot.kernelModules = [ "kvm-amd" ];
-  boot.extraModulePackages = [ ];
-
-  fileSystems."/" =
-    { device = "/dev/disk/by-label/nixos";
-      fsType = "ext4";
+  boot = {
+    initrd = {
+      availableKernelModules = [
+        "xhci_pci"
+        "ahci"
+        "ehci_pci"
+        "nvme"
+        "usb_storage"
+        "usbhid"
+        "uas"
+        "sd_mod"
+      ];
+      kernelModules = [ "dm-snapshot" ];
     };
+    kernelModules = [ "kvm-amd" ];
+    extraModulePackages = [ ];
+  };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-label/swap"; }
-    ];
+  nix.settings.max-jobs = lib.mkDefault 4;
+
+  powerManagement.cpuFreqGovernor = "powersave";
+
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
   hardware.video.hidpi.enable = lib.mkDefault true;
 }
