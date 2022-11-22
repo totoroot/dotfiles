6f74499ce912a14642c9503cedbed6fba57c4f38
Matthias Thym <git@thym.at>
2022-11-22 11:48:06 +0100

    Reformat comments in documents module

--- modules/desktop/documents.nix
+++ modules/desktop/documents.nix
@@ -12,17 +12,28 @@ in {
 
   config = mkIf cfg.enable {
     user.packages = with pkgs; [
-      libreoffice-fresh   # install latest version of libre office
-      pdf2odt             # pdf to odt/ods converter
-      zathura             # pdf viewer
-      qpdfview            # advanced pdf viewer for forms and annotations
-      unstable.pdfcpu     # pdf processor
-      pdfgrep             # grep for pdf
-      pdfpc      # pdf presenter console
-      pdfsandwich         # ocr for pdf
-      wkhtmltopdf         # render html to pdf
-      xournalpp           # handwriting notetaking software with PDF annotation support
-      xfce.mousepad       # simple text editor for Xfce
+      # Comprehensive, professional-quality productivity suite, a variant of openoffice.org
+      libreoffice-fresh
+      # PDF to ODT/ODS converter
+      pdf2odt
+      # Highly customizable and functional PDF viewer
+      zathura
+      # Advanced PDF viewer for forms and annotated documents
+      qpdfview
+      # CLI PDF processor
+      pdfcpu
+      # CLI utility to search text in PDF files
+      pdfgrep
+      # Presenter console with multi-monitor support for PDF files
+      pdfpc
+      # OCR tool for scanned PDFs
+      pdfsandwich
+      # Tools for rendering web pages to PDF or images
+      wkhtmltopdf
+      # Handwriting notetaking software with PDF annotation support
+      xournalpp
+      # Simple GUI text editor
+      xfce.mousepad
     ];
   };
 }
