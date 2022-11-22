18126abec06ec644972c70eb37ad818a047e4c17
Matthias Thym <git@thym.at>
2022-11-22 12:41:56 +0100

    Reformat purple hardware config and set cpuiFreqGovernor to ondemand

--- hosts/purple/hardware-configuration.nix
+++ hosts/purple/hardware-configuration.nix
@@ -1,13 +1,25 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
 
   boot = {
-    initrd.availableKernelModules = [ "nvme" "ahci" "ohci_pci" "ehci_pci" "xhci_pci" "usb_storage" "usbhid" "uas" "sd_mod" ];
-    initrd.kernelModules = [];
+    initrd = {
+      availableKernelModules = [
+        "nvme"
+        "ahci"
+        "ohci_pci"
+        "ehci_pci"
+        "xhci_pci"
+        "usb_storage"
+        "usbhid"
+        "uas"
+        "sd_mod"
+      ];
+      kernelModules = [];
+    };
     extraModulePackages = [];
     kernelModules = [
       "kvm-amd"
@@ -18,14 +30,11 @@
       "fam15h_power"
       "amdgpu"
     ];
-    # kernelParams = [
-      # "video=HDMI-1:3440x1440@60"
-    # ];
   };
 
   nix.settings.max-jobs = lib.mkDefault 16;
 
-  powerManagement.cpuFreqGovernor = "performance";
+  powerManagement.cpuFreqGovernor = "ondemand";
 
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
