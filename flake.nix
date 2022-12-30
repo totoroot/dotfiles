cecf9e884ad942484aec0d60eac7c3308cd71c6f
Matthias Thym <git@thym.at>
2022-12-30 09:17:37 +0100

    Add devenv to flake config

--- flake.nix
+++ flake.nix
@@ -1,31 +1,31 @@
-# flake.nix --- the heart of my dotfiles
+# flake.nix --- Where the journey to a declarative system starts
 #
-# Author:  Henrik Lissner <henrik@lissner.net>
-# URL:     https://github.com/hlissner/dotfiles
+# Author:  Matthias Thym <git@thym.at>
+# URL:     https://codeberg.org/totoroot/dotfiles
 # License: MIT
-#
-# Welcome to ground zero. Where the whole flake gets set up and all its modules
-# are loaded.
+
+# If you have no idea what flakes are and what they are used for, then do like I
+# did and read up on it with the following resources:
+# Flakes in NixOS wiki: https://nixos.wiki/wiki/Flakes
+# Nix Flake in NixOS manual: https://nixos.org/manual/nix/stable/command-ref/new-cli/nix3-flake.html
+# Examples for getting started: https://jdisaacs.com/blog/nixos-config/
+# Article by tweag.io on Nix flakes: https://www.tweag.io/blog/2020-07-31-nixos-flakes/
 
 {
-  description = "A grossly incandescent NixOS config.";
+  description = "totoroot's NixOS flake";
 
   inputs =
     {
-      # Core dependencies.
       nixos.url = "nixpkgs/nixos-unstable";
       nixos-unstable.url = "nixpkgs/nixos-unstable";
-
       nixpkgs.url = "nixpkgs/master";
-
       home-manager.url = "github:rycee/home-manager/release-22.05";
       home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-      # Extras
       nixos-hardware.url = "github:nixos/nixos-hardware";
+      devenv.url = "github:cachix/devenv/v0.4";
     };
 
-  outputs = inputs @ { self, nixos, nixos-unstable, nixpkgs, home-manager, ... }:
+  outputs = inputs @ { self, nixos, nixos-unstable, nixpkgs, home-manager, devenv, ... }:
     let
       inherit (builtins) baseNameOf;
       inherit (lib) nixosSystem mkIf removeSuffix attrNames attrValues;
@@ -38,25 +38,28 @@
 
       mkPkgs = pkgs: extraOverlays: import pkgs {
         inherit system;
-        config.allowUnfree = true;  # forgive me Stallman senpai
+        # Allow the use of unfree software
+        config.allowUnfree = true;
         overlays = extraOverlays ++ (attrValues self.overlays);
       };
+      # Use different channels for installed packages for increased flexibility
       pkgs  = mkPkgs nixos [ self.overlay ];
       unstable = mkPkgs nixos-unstable [];
-
     in {
       lib = lib.my;
 
       overlay =
         final: prev: {
           inherit unstable;
-          user = self.packages."${system}";
+          # Add overlays for flakes
+          user = self.packages.${system};
+          devenv = devenv.packages.${system}.devenv; 
         };
 
       overlays =
         mapModules ./overlays import;
 
-      packages."${system}" =
+      packages.${system} =
         mapModules ./packages
           (p: pkgs.callPackage p {});
 
@@ -68,7 +71,7 @@
         mapHosts ./hosts { inherit system; };
 
 
-      devShell."${system}" =
+      devShell.${system} =
         import ./shell.nix { inherit pkgs; };
     };
 }
