b284c07061e92897c2f8db371d12435611b98118
Matthias Thym <git@thym.at>
2023-04-02 14:07:34 +0200

    Add i18n settings to personal defaults

--- hosts/personal.nix
+++ hosts/personal.nix
@@ -25,6 +25,19 @@ with lib;
   ## Location config -- since Vienna is my 127.0.0.1
   time.timeZone = mkDefault "Europe/Vienna";
   i18n.defaultLocale = mkDefault "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "de_AT.UTF-8";
+    LC_IDENTIFICATION = "de_AT.UTF-8";
+    LC_MEASUREMENT = "de_AT.UTF-8";
+    LC_MONETARY = "de_AT.UTF-8";
+    LC_NAME = "de_AT.UTF-8";
+    LC_NUMERIC = "de_AT.UTF-8";
+    LC_PAPER = "de_AT.UTF-8";
+    LC_TELEPHONE = "de_AT.UTF-8";
+    LC_TIME = "de_AT.UTF-8";
+  };
+
   # For redshift, mainly
   # Go try find my home with those coordinates
   # as those are randomly picked within a range near my home
