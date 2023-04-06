716e1d19584eb5d37603d98a2c1b37fb9a629ca1
Matthias Thym <git@thym.at>
2023-04-06 19:51:56 +0200

    Disable Hyprland environment variables to make it work

--- modules/desktop/environments/hyprland.nix
+++ modules/desktop/environments/hyprland.nix
@@ -118,25 +118,28 @@ in {
       # gtkUsePortal = true;
     };
 
-    environment.sessionVariables = rec {
-      CLUTTER_BACKEND = "wayland";
-      XDG_SESSION_TYPE = "wayland";
-      QT_QPA_PLATFORM = "wayland";
-      SDL_VIDEODRIVER = "wayland";
-      QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
-      MOZ_ENABLE_WAYLAND = "1";
-      WLR_NO_HARDWARE_CURSORS = "1";
-      WLR_BACKEND = "vulkan";
-      # GDK_BACKEND = "wayland";
-      XDG_SESSION_DESKTOP = "Hyprland";
-      # QT_QPA_PLATFORMTHEME = "gtk2";
-      # Fix for some Java AWT applications (e.g. Android Studio),
-      # use this if they aren't displayed properly:
-      _JAVA_AWT_WM_NONREPARENTING = "1";
-      # Better Wayland support for Electron-based apps
-      # https://discourse.nixos.org/t/partly-overriding-a-desktop-entry/20743/2?u=totoroot
-      NIXOS_OZONE_WL = "1";
-    };
+    # Make Hyprland session available if a display manager like SDDM is enabled
+    services.xserver.displayManager.sessionPackages = [ pkgs.hyprland ];
+
+    # environment.sessionVariables = rec {
+      # CLUTTER_BACKEND = "wayland";
+      # XDG_SESSION_TYPE = "wayland";
+      # QT_QPA_PLATFORM = "wayland";
+      # SDL_VIDEODRIVER = "wayland";
+      # QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+      # MOZ_ENABLE_WAYLAND = "1";
+      # WLR_NO_HARDWARE_CURSORS = "1";
+      # WLR_BACKEND = "vulkan";
+      # # GDK_BACKEND = "wayland";
+      # XDG_SESSION_DESKTOP = "Hyprland";
+      # # QT_QPA_PLATFORMTHEME = "gtk2";
+      # # Fix for some Java AWT applications (e.g. Android Studio),
+      # # use this if they aren't displayed properly:
+      # _JAVA_AWT_WM_NONREPARENTING = "1";
+      # # Better Wayland support for Electron-based apps
+      # # https://discourse.nixos.org/t/partly-overriding-a-desktop-entry/20743/2?u=totoroot
+      # NIXOS_OZONE_WL = "1";
+    # };
 
     services = {
       # dbus.enable = true;
