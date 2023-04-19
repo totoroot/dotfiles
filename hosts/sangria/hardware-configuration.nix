6d25019571396e2dac4f9410c1a463baa39bffee
Matthias Thym <git@thym.at>
2023-04-19 22:47:57 +0200

    Fix messed up hardware config for sangria

--- hosts/sangria/hardware-configuration.nix
+++ hosts/sangria/hardware-configuration.nix
@@ -1,5 +1,4 @@
 { config, lib, modulesPath, ... }:
-
 {
   imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
@@ -25,7 +24,7 @@
     loader.efi.efiSysMountPoint = "/boot/efi";
   };
 
-  powerManagement.cpuFreqGovernor = "powersave";
+  powerManagement.cpuFreqGovernor = "ondemand";
 
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
