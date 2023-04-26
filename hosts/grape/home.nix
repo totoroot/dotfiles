991b2257d528101f2b02a459053ae1fa6b4de411
Matthias Thym <git@thym.at>
2023-04-26 12:44:13 +0200

    Sync notes folder with Nextcloud

--- hosts/grape/home.nix
+++ hosts/grape/home.nix
@@ -3,7 +3,7 @@
   home.file = {
     "dev/.use".text = "development";
     "dls/.use".text = "downloads";
-    "nts/.use".text = "notes";
+    "nts/".source = "/home/mathym/sync/notes/";
     "tmp/.use".text = "temporary files";
     "trash/".source = "/home/mathym/.local/share/Trash/files/";
   };
