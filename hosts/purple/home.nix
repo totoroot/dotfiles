22e90ec6e6f4816b90406297cc3cc21e7cbf3aa5
Matthias Thym <git@thym.at>
2023-04-26 23:58:53 +0200

    Restructure home directories

--- hosts/purple/home.nix
+++ hosts/purple/home.nix
@@ -3,10 +3,14 @@
   home.file = {
     "Desktop/.use".text = "desktop";
     "Dev/.use".text = "development";
+    "Documents/.use".text = "documents";
     "Downloads/.use".text = "downloads";
+    "Games/.use".text = "games";
+    "Music/.use".text = "music";
     "Notes/".source = "/home/mathym/Sync/notes/";
     "Pictures/.use".text = "photos and graphics";
     "Public/.use".text = "shared files";
+    "Resources/.use".text = "resources";
     "Sync/.use".text = "synchronised files";
     "Sync/notes.use".text = "notes";
     "Temp/.use".text = "temporary files";
