de38e9518f0e11fe31a1a753eba3e2a4f6256284
Matthias Thym <git@thym.at>
2023-07-10 11:12:59 +0200

    Add hardware setting recommendations from nixos-hardware for moooh

--- hosts/moooh/hardware-configuration.nix
+++ hosts/moooh/hardware-configuration.nix
@@ -17,7 +17,10 @@
         "thunderbolt"
         "vmd"
       ];
-      kernelModules = [ "dm-snapshot" ];
+      kernelModules = [
+        "dm-snapshot"
+        "i915"
+      ];
       luks.devices."nixos" = {
         device = "/dev/disk/by-uuid/47d8eafe-895e-474d-884a-de19eaf31a6c";
         preLVM = true;
@@ -28,7 +31,19 @@
         "keyfile.bin" = "/etc/secrets/initrd/keyfile.bin";
       };
     };
-    kernelModules = [ "kvm-intel" ];
+    kernelModules = [
+      "kvm-intel"
+      "1915"
+    ];
+    kernelParams = [
+      # needed for Intel Iris Xe
+      "i915.force_probe=46a8"
+      "i915.enable_guc=3"
+      "i915.fastboot=1"
+      # needed for keyboard
+      "i8042.dumbkbd=1"
+      "i8042.nopnp=1"
+    ];
     extraModulePackages = [ ];
     loader = {
       efi = {
@@ -39,16 +54,27 @@
         enable = true;
         configurationLimit = 10;
       };
-        # grub = {
-          # enable = true;
-          # device = "nodev";
-          # efiSupport = true;
-          # enableCryptodisk = true;
-        # };
     };
   };
 
-  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+  powerManagement.cpuFreqGovernor = "powersave";
 
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.intel.updateMicrocode = config.hardware.enableRedistributableFirmware;
+
+  hardware.opengl.extraPackages = with pkgs; [
+    vaapiIntel
+    libvdpau-va-gl
+    intel-media-driver
+  ];
+
+  services.xserver.videoDrivers = [ "intel" ];
+
+  services.tlp.enable = ((lib.versionOlder (lib.versions.majorMinor lib.version) "21.05")
+                                         || !config.services.power-profiles-daemon.enable);
+
+  services.fstrim.enable = true;
+
+  environment.variables = {
+    VDPAU_DRIVER = lib.mkIf config.hardware.opengl.enable (lib.mkDefault "va_gl");
+  };
 }
