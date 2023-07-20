7155a071a2e201bda0f1a464a685e180b5f4c913
Matthias Thym <git@thym.at>
2023-07-20 13:43:58 +0200

    Add faster nix-direnv

--- modules/shell/direnv.nix
+++ modules/shell/direnv.nix
@@ -11,6 +11,7 @@ in {
   config = mkIf cfg.enable {
     user.packages = with pkgs; [
       direnv
+      nix-direnv
       devenv
     ];
     modules.shell.zsh.rcInit = ''eval "$(direnv hook zsh)"'';
