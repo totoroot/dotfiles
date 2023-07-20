944186c123aa5234925ed19697dad96e9f71ea9d
Matthias Thym <git@thym.at>
2023-07-20 13:43:23 +0200

    Ignore eurkey layout files for search

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -22,7 +22,7 @@ alias mv='mv -v'
 alias mkdir='mkdir -p'
 alias wget='wget -c'
 alias cat='bat -Pp'
-alias search='grep -ir --exclude-dir=".git" --exclude="*.html" --exclude="*.css" --exclude="*.scss" --exclude="*.js" --exclude="*.json"'
+alias search='grep -ir --exclude-dir=".git" --exclude="*.html" --exclude="*.css" --exclude="*.scss" --exclude="*.js" --exclude="*.json" --exclude="~/.config/dotfiles/config/eurkey/*"'
 alias find='fd'
 alias replace='sd'
 
