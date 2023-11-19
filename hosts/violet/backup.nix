1c56a89feb918466d1a4e9f3b01f19e2ab3f543b
Matthias Thym <git@thym.at>
2023-11-19 15:16:27 +0100

    Update syncthing settings

--- hosts/violet/backup.nix
+++ hosts/violet/backup.nix
@@ -1,4 +1,5 @@
 { pkgs, ... }:
+
 {
   # This only exposes the service to purple
   networking.firewall.extraCommands = ''
@@ -20,23 +21,23 @@
     # More information on Syncthing's firewall config can be found here:
     # https://docs.syncthing.net/users/firewall.html    services.syncthing.
     guiAddress = "0.0.0.0:8384";
-
     # Overrides any devices added or deleted through the WebUI
     overrideDevices = false;
     # Overrides any folders added or deleted through the WebUI
     overrideFolders = false;
+    settings = {
+      devices = {
+        purple.id = "C6E5H63-KPO3NWZ-WMIY4TN-PHQGLXQ-ZLQC2WL-YOX3BG2-BIFJ6RV-O2QKWAU";
+        violet.id = "ESP4NJ3-KP3QB7K-TN6G6S2-7PLQNB5-7CFQGVQ-DRICFJ2-PHGKTAO-GDHOGAR";
+        lilac.id = "4K4Q7HM-KLYVORI-QPVOZKJ-7Y27FSE-DU4M4D6-5PU6HAB-ZLHJJK4-6AQVNAS";
+        phone.id = "CHUU3Y7-XQ2MHGU-XYLHRQW-CZ6QW6C-WAG6R4A-XDW2Z3W-XBT7BLK-GE25KQO";
+      };
 
-    devices = {
-      purple.id = "C6E5H63-KPO3NWZ-WMIY4TN-PHQGLXQ-ZLQC2WL-YOX3BG2-BIFJ6RV-O2QKWAU";
-      violet.id = "ESP4NJ3-KP3QB7K-TN6G6S2-7PLQNB5-7CFQGVQ-DRICFJ2-PHGKTAO-GDHOGAR";
-      lilac.id = "4K4Q7HM-KLYVORI-QPVOZKJ-7Y27FSE-DU4M4D6-5PU6HAB-ZLHJJK4-6AQVNAS";
-      phone.id = "CHUU3Y7-XQ2MHGU-XYLHRQW-CZ6QW6C-WAG6R4A-XDW2Z3W-XBT7BLK-GE25KQO";
-    };
-
-    folders = {
-      "phone-backups" = {
-        path = "/mnt/data/backups/phone";
-        devices = [ "phone" ];
+      folders = {
+        "phone-backups" = {
+          path = "/mnt/data/backups/phone";
+          devices = [ "phone" ];
+        };
       };
     };
   };
