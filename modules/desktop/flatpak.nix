a42f67f8f902af25252eb466c637e7a56f0f3e80
Matthias Thym <git@thym.at>
2023-12-02 12:15:45 +0100

    Update flathub URL

--- modules/desktop/flatpak.nix
+++ modules/desktop/flatpak.nix
@@ -29,7 +29,7 @@ in {
       wantedBy = [ "multi-user.target" ];
       path = [ pkgs.flatpak ];
       script = ''
-        flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+        flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
       '';
     };
   };
