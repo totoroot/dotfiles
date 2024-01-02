4f0d1b86ff9c7ac418f9c354d8cf4e5e11a6dcfb
Matthias Thym <git@thym.at>
2024-01-02 17:37:02 +0000

    Try to fix Websockets for NGINX route for Uptime Kuma

--- modules/services/nginx.nix
+++ modules/services/nginx.nix
@@ -69,8 +69,15 @@ in
           enableACME = true;
           forceSSL = true;
           locations."/" = {
-            proxyPass = "http://localhost:${toString uptimePort}";
-            proxyWebsockets = true;
+            extraConfig = ''
+              proxy_pass http://localhost:${toString uptimePort};
+              proxy_set_header Host $host;
+              proxy_redirect http:// https://;
+              proxy_http_version 1.1;
+              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+              proxy_set_header Upgrade $http_upgrade;
+              proxy_set_header Connection $connection_upgrade;
+            '';
           };
         };
         # violet
