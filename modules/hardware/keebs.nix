fd8e01aa10a00bdc0c83ba56ffa42ee8aa189e5b
Matthias Thym <git@thym.at>
2024-03-04 13:40:36 +0100

    Formatting/remove hard tabs

--- modules/hardware/keebs.nix
+++ modules/hardware/keebs.nix
@@ -14,14 +14,12 @@ in {
     ];
 
     # See https://github.com/zsa/wally/wiki for more info
-    services.udev = {
-      extraRules = ''
-        # STM32 rules for the ZSA Moonlander keyboard
-        SUBSYSTEMS=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="df11", MODE:="0666", SYMLINK+="stm32_dfu"
-        # Oryx live training rule for ZSA Moonlander keyboard
-        SUBSYSTEM=="usb", ATTR{idVendor}=="3297", ATTR{idProduct}=="1969", GROUP="plugdev"
-      '';
-    };
+    services.udev.extraRules = ''
+      # STM32 rules for the ZSA Moonlander keyboard
+      SUBSYSTEMS=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="df11", MODE:="0666", SYMLINK+="stm32_dfu"
+      # Oryx live training rule for ZSA Moonlander keyboard
+      SUBSYSTEM=="usb", ATTR{idVendor}=="3297", ATTR{idProduct}=="1969", GROUP="plugdev"
+    '';
 
     user.extraGroups = [ "plugdev" ];
   };
