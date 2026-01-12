48cab47d11c01fd1c6202b957fa9559b8c9bbbba
Matthias Thym <git@thym.at>
2026-01-12 21:33:33 +0100

    fix: moooh config

--- hosts/moooh/hardware-configuration.nix
+++ hosts/moooh/hardware-configuration.nix
@@ -1,158 +1,156 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 let
-  root_device_uuid = "/dev/disk/by-uuid/"
+  boot_device_uuid = "/dev/disk/by-uuid/";
+  root_device_uuid = "/dev/disk/by-uuid/";
+in
 
-    {
-      imports =
-        [
-          (modulesPath + "/installer/scan/not-detected.nix")
-        ];
+{
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
 
-      boot = {
-        initrd = {
-          availableKernelModules = [
-            "xhci_pci"
-            "nvme"
-            "uas"
-            "usb_storage"
-            "usbhid"
-            "sd_mod"
-            "thunderbolt"
-            "vmd"
-            "aesni_intel"
-            "cryptd"
-          ];
-          kernelModules = [
-            "dm-snapshot"
-            "i915"
-          ];
-          luks.devices."nixos" = {
-            device = "${root_device_uuid}";
-            # preLVM = true;
-            allowDiscards = true;
-          };
-        };
-        resumeDevice = "${root_device_uuid}";
-        supportedFilesystems = [ "btrfs" ];
-        kernelModules = [
-          "kvm-intel"
-          "1915"
-        ];
-        kernelParams = [
-          # Something, something swapfile
-          "resume_offset=101861376"
-          # needed for Intel Iris Xe
-          "i915.force_probe=46a8"
-          "i915.enable_guc=3"
-          "i915.fastboot=1"
-          # needed for keyboard
-          "i8042.dumbkbd=1"
-          "i8042.nopnp=1"
-        ];
-        extraModulePackages = [ ];
-        loader = {
-          efi = {
-            canTouchEfiVariables = true;
-            efiSysMountPoint = "/boot";
-          };
-          systemd-boot = {
-            enable = true;
-            configurationLimit = 10;
-          };
-        };
+  boot = {
+    initrd = {
+      availableKernelModules = [
+        "xhci_pci"
+        "nvme"
+        "uas"
+        "usb_storage"
+        "usbhid"
+        "sd_mod"
+        "thunderbolt"
+        "vmd"
+        "aesni_intel"
+        "cryptd"
+      ];
+      kernelModules = [
+        "dm-snapshot"
+        "i915"
+      ];
+      luks.devices."nixos" = {
+        device = "${root_device_uuid}";
+        # preLVM = true;
+        allowDiscards = true;
       };
-
-      powerManagement = {
+    };
+    resumeDevice = "${root_device_uuid}";
+    supportedFilesystems = [ "btrfs" ];
+    kernelModules = [
+      "kvm-intel"
+      "1915"
+    ];
+    kernelParams = [
+      # Something, something swapfile
+      "resume_offset=101861376"
+      # needed for Intel Iris Xe
+      "i915.force_probe=46a8"
+      "i915.enable_guc=3"
+      "i915.fastboot=1"
+      # needed for keyboard
+      "i8042.dumbkbd=1"
+      "i8042.nopnp=1"
+    ];
+    extraModulePackages = [ ];
+    loader = {
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot";
+      };
+      systemd-boot = {
         enable = true;
-        powertop.enable = true;
-        cpuFreqGovernor = "powersave";
+        configurationLimit = 10;
       };
+    };
+  };
 
-      services.auto-cpufreq = {
-        # Failed assertions:
-        # - You have set services.power-profiles-daemon.enable = true;
-        # which conflicts with services.auto-cpufreq.enable = true;
-        enable = false;
-        # settings = {
-        #   battery = {
-        #     governor = "powersave";
-        #     turbo = "never";
-        #   };
-        #   charger = {
-        #     governor = "performance";
-        #     turbo = "auto";
-        #   };
-        # };
-      };
+  powerManagement = {
+    enable = true;
+    powertop.enable = true;
+    cpuFreqGovernor = "powersave";
+  };
 
-      hardware.cpu.intel.updateMicrocode = config.hardware.enableRedistributableFirmware;
+  services.auto-cpufreq = {
+    # Failed assertions:
+    # - You have set services.power-profiles-daemon.enable = true;
+    # which conflicts with services.auto-cpufreq.enable = true;
+    enable = false;
+    # settings = {
+    #   battery = {
+    #     governor = "powersave";
+    #     turbo = "never";
+    #   };
+    #   charger = {
+    #     governor = "performance";
+    #     turbo = "auto";
+    #   };
+    # };
+  };
 
-      hardware.graphics.extraPackages = with pkgs; [
-        intel-vaapi-driver
-        libvdpau-va-gl
-        intel-media-driver
-      ];
+  hardware.cpu.intel.updateMicrocode = config.hardware.enableRedistributableFirmware;
 
-      # services.fprintd = {
-      #   enable = true;
-      #   package = pkgs.fprintd-tod;
-      #   tod = {
-      #     enable = true;
-      #     driver = pkgs.libfprint-2-tod1-broadcom;
-      #   };
-      # };
+  hardware.graphics.extraPackages = with pkgs; [
+    intel-vaapi-driver
+    libvdpau-va-gl
+    intel-media-driver
+  ];
 
-      services.fstrim.enable = true;
+  # services.fprintd = {
+  #   enable = true;
+  #   package = pkgs.fprintd-tod;
+  #   tod = {
+  #     enable = true;
+  #     driver = pkgs.libfprint-2-tod1-broadcom;
+  #   };
+  # };
 
-      environment.variables = {
-        VDPAU_DRIVER = lib.mkIf config.hardware.graphics.enable (lib.mkDefault "va_gl");
-      };
+  services.fstrim.enable = true;
 
-      programs.fuse.userAllowOther = true;
+  environment.variables = {
+    VDPAU_DRIVER = lib.mkIf config.hardware.graphics.enable (lib.mkDefault "va_gl");
+  };
 
-      environment.systemPackages = with pkgs; [
-        # Mount remote directories over SSH
-        sshfs
-      ];
+  programs.fuse.userAllowOther = true;
 
-      fileSystems = {
-        "/" = {
-          device = "/dev/disk/by-label/nixos";
-          fsType = "btrfs";
-          options = [ "subvol=root" "compress=zstd" "noatime" ];
-        };
-        "/home" = {
-          device = "/dev/disk/by-label/nixos";
-          fsType = "btrfs";
-          options = [ "subvol=home" "compress=zstd" "noatime" ];
-        };
-        "/nix" = {
-          device = "/dev/disk/by-label/nixos";
-          fsType = "btrfs";
-          options = [ "subvol=nix" "compress=zstd" "noatime" ];
-        };
-        "/persist" = {
-          device = "/dev/disk/by-label/nixos";
-          fsType = "btrfs";
-          options = [ "subvol=persist" "compress=zstd" "noatime" ];
-          neededForBoot = true;
-        };
-        "/var/log" = {
-          device = "/dev/disk/by-label/nixos";
-          fsType = "btrfs";
-          options = [ "subvol=log" "compress=zstd" "noatime" ];
-          # Needed for correct log order
-          neededForBoot = true;
-        };
-        "/boot/efi" = {
-          device = "/dev/disk/by-uuid/2572-BEF2";
-          fsType = "vfat";
-        };
+  environment.systemPackages = with pkgs; [
+    # Mount remote directories over SSH
+    sshfs
+  ];
 
-        swapDevices = [
-          {
-            device = "/swap";
-          }
-        ];
-      }
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/nixos";
+      fsType = "btrfs";
+      options = [ "subvol=root" "compress=zstd" "noatime" ];
+    };
+    "/home" = {
+      device = "/dev/disk/by-label/nixos";
+      fsType = "btrfs";
+      options = [ "subvol=home" "compress=zstd" "noatime" ];
+    };
+    "/nix" = {
+      device = "/dev/disk/by-label/nixos";
+      fsType = "btrfs";
+      options = [ "subvol=nix" "compress=zstd" "noatime" ];
+    };
+    "/persist" = {
+      device = "/dev/disk/by-label/nixos";
+      fsType = "btrfs";
+      options = [ "subvol=persist" "compress=zstd" "noatime" ];
+      neededForBoot = true;
+    };
+    "/var/log" = {
+      device = "/dev/disk/by-label/nixos";
+      fsType = "btrfs";
+      options = [ "subvol=log" "compress=zstd" "noatime" ];
+      # Needed for correct log order
+      neededForBoot = true;
+    };
+    "/boot/efi" = {
+      device = "${boot_device_uuid}";
+      fsType = "vfat";
+    };
+  };
+
+  swapDevices = [{ device = "/swap"; }];
+}
