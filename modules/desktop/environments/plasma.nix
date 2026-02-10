192f10273549d3b68964568bca6beb4742d42f0e
Matthias Thym <git@thym.at>
2026-02-11 00:45:36 +0100

    plasma: force KDE SDDM package

--- modules/desktop/environments/plasma.nix
+++ modules/desktop/environments/plasma.nix
@@ -15,7 +15,7 @@ in {
         # Make it rewritable in case Plasma should be installed
         # but other display manager should be used
         enable = mkDefault true;
-        package = mkDefault pkgs.kdePackages.sddm;
+        package = mkForce pkgs.kdePackages.sddm;
         # Use Wayland Session by default
         wayland = {
           enable = mkDefault true;
