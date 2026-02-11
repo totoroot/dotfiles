74f015953a60d415159c4dec43e0c5bb909295d4
Matthias Thym <git@thym.at>
2026-02-11 01:48:20 +0100

    attic: disable watcher for now

--- hosts/purple/default.nix
+++ hosts/purple/default.nix
@@ -12,7 +12,7 @@
     nix.atticCache = {
       enableServer = true;
       enableClient = true;
-      enableWatcher = true;
+      enableWatcher = false;
       host = "purple-ts";
       port = 5129;
       # Set to the public key for your cache, e.g. "cache-name:BASE64"
