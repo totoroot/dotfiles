72d3a0f8d22957ec2b35edf2281114d2c805cac8
Matthias Thym <git@thym.at>
2026-02-13 12:29:41 +0100

    fix: add purple host

---
+++ hosts/nixos/purple/mounts.nix
@@ -0,0 +1,119 @@
+{ pkgs, ... }:
+
+{
+  programs.fuse.userAllowOther = true;
+
+  environment.systemPackages = with pkgs; [
+    # Mount remote directories over SSH
+    sshfs
+    # Mount WebDAV shares
+    davfs2
+  ];
+
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/nixos";
+      fsType = "ext4";
+    };
+    "/boot" = {
+      device = "/dev/disk/by-label/boot";
+      fsType = "vfat";
+    };
+    "/home" = {
+      device = "/dev/disk/by-uuid/1f70506a-3629-473e-8bf3-b5033ce42450";
+      fsType = "btrfs";
+      options = [ "noatime" "recovery" ];
+    };
+    "/mnt/backup" = {
+      device = "/dev/disk/by-label/backup";
+      fsType = "ext4";
+      options = [ "noatime" ];
+    };
+    # "/mnt/violet" = {
+    #   device = "mathym@violet:";
+    #   fsType = "sshfs";
+    #   options = [
+    #     # Filesystem options
+    #     # For non-root access
+    #     "allow_other"
+    #     # This is a network fs
+    #     "_netdev"
+    #     # Mount on demand
+    #     "x-systemd.automount"
+    #     # Prevent long "start job is running" wait times
+    #     "x-systemd.device-timeoup=1ms"
+    #     "follow_symlinks"
+    #     # SSH options
+    #     # Handle connection drops
+    #     "reconnect"
+    #     # Keep connections alive
+    #     "ServerAliveInterval=15"
+    #     "ServerAliveCountMax=3"
+    #     "IdentityFile=/home/mathym/.ssh/purple"
+    #     # Troubleshoot connection
+    #     # "sshfs_debug"
+    #     # Debugging options
+    #     "debug"
+    #     "loglevel=debug"
+    #   ];
+    # };
+    # "/mnt/photos" = {
+    #   device = "mathym@violet:/mnt/photos";
+    #   fsType = "sshfs";
+    #   options = [
+    #     # Filesystem options
+    #     # For non-root access
+    #     "allow_other"
+    #     # This is a network fs
+    #     "_netdev"
+    #     # Mount on demand
+    #     "x-systemd.automount"
+    #     # Prevent long "start job is running" wait times
+    #     "x-systemd.device-timeoup=1ms"
+    #     "follow_symlinks"
+    #     # SSH options
+    #     # Handle connection drops
+    #     "reconnect"
+    #     # Keep connections alive
+    #     "ServerAliveInterval=15"
+    #     "ServerAliveCountMax=3"
+    #     "IdentityFile=/home/mathym/.ssh/purple"
+    #     # Troubleshoot connection
+    #     # "sshfs_debug"
+    #     # Debugging options
+    #     "debug"
+    #     "loglevel=debug"
+    #   ];
+    # };
+    # "/mnt/music" = {
+    # device = "mathym@violet:/mnt/music";
+    # fsType = "sshfs";
+    # options = [
+    # # Filesystem options
+    # # For non-root access
+    # "allow_other"
+    # # This is a network fs
+    # "_netdev"
+    # # Mount on demand
+    # "x-systemd.automount"
+    # # Prevent long "start job is running" wait times
+    # "x-systemd.device-timeoup=1ms"
+    # "follow_symlinks"
+    # # SSH options
+    # # Handle connection drops
+    # "reconnect"
+    # # Keep connections alive
+    # "ServerAliveInterval=15"
+    # "ServerAliveCountMax=3"
+    # "IdentityFile=/home/mathym/.ssh/purple"
+    # # Troubleshoot connection
+    # # "sshfs_debug"
+    # # Debugging options
+    # "debug"
+    # "loglevel=debug"
+    # ];
+    # };
+  };
+
+  swapDevices = [{ device = "/dev/disk/by-label/swap"; }];
+}
