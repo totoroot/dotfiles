5bf3d852143abe5ecc502ae2e8a5dc5418bfd147
Matthias Thym <git@thym.at>
2026-02-13 15:24:01 +0100

    adventurelog: drop db/postgis overrides

--- modules/services/adventurelog.nix
+++ modules/services/adventurelog.nix
@@ -58,15 +58,6 @@ in
     };
 
     services.postgresql.package = lib.mkForce pkgs.postgresql_16;
-    services.postgresql.extensions =
-      lib.mkForce [ config.services.postgresql.package.pkgs.postgis ];
-    services.postgresql.ensureDatabases = lib.mkAfter [ "adventurelog" ];
-    services.postgresql.ensureUsers = lib.mkAfter [
-      {
-        name = "adventurelog";
-        ensureDBOwnership = true;
-      }
-    ];
 
     security.acme = {
       acceptTerms = true;
@@ -83,21 +74,5 @@ in
       "d /var/lib/adventurelog/media 0750 adventurelog adventurelog -"
     ];
 
-    systemd.services.adventurelog-postgis = {
-      description = "Ensure PostGIS extension for AdventureLog";
-      after = [ "postgresql.service" ];
-      requires = [ "postgresql.service" ];
-      wantedBy = [ "multi-user.target" ];
-      serviceConfig = {
-        Type = "oneshot";
-        User = "postgres";
-        Group = "postgres";
-      };
-      script = ''
-        ${config.services.postgresql.package}/bin/psql \
-          --dbname=adventurelog \
-          --command "CREATE EXTENSION IF NOT EXISTS postgis;"
-      '';
-    };
   };
 }
