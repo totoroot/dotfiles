9ebaa8e7dbfd725ec4540824290b48cda0f56a82
Matthias Thym <git@thym.at>
2026-02-15 18:35:05 +0100

    rebuild: add host-guarded nrswitch wrapper

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -42,6 +42,7 @@ alias nixinst='(){ nix profile install nixpkgs#$1 ;}'
 # alias nixrm='nix-env --uninstall'
 alias nixfix='sudo nix-store --verify --check-contents --repair'
 alias nixedit='(){ $EDITOR $(fd $1)/default.nix ;}'
+alias nrs='nrswitch'
 
 # edit zshrc
 alias zshconfig="$EDITOR ~/.zshrc"
