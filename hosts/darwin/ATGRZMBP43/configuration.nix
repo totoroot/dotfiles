60985a3204e272203ec21666c262289afd7d7be4
Matthias Thym <git@thym.at>
2026-02-16 18:06:51 +0100

    chore(darwin): restore lix overlays and caches

--- hosts/darwin/ATGRZMBP43/configuration.nix
+++ hosts/darwin/ATGRZMBP43/configuration.nix
@@ -16,14 +16,28 @@
 
   # Nix configuration ------------------------------------------------------------------------------
 
+  nixpkgs.overlays = [ (final: prev: {
+    inherit (prev.lixPackageSets.latest)
+      nixpkgs-review
+      nix-eval-jobs
+      nix-fast-build
+      colmena;
+  }) ];
+
   nix = {
+    # Use Lix
+    package = pkgs.lixPackageSets.latest.lix;
+
     settings = {
-      # binaryCaches = [ "https://cache.nixos.org/" ];
-      # binaryCachePublicKeys = [
-      # "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      # ];
-      # trustedUsers = [ "@admin" ];
-      trusted-substituters = [ "https://devenv.cachix.org" ];
+      substituters = [
+        "https://cache.nixos.org/"
+        "https://devenv.cachix.org"
+        "https://nixpkgs-python.cachix.org"
+      ];
+      trusted-public-keys = [
+        "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+        "nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU="
+      ];
     };
 
     # Enable experimental nix command and flakes
