009630909ae0408cc32b0508a8abbde7f92f1666
Matthias Thym <git@thym.at>
2026-02-16 21:39:18 +0100

    feat(home): extend nushell with plugins and formatter

--- home/modules/nushell.nix
+++ home/modules/nushell.nix
@@ -8,16 +8,33 @@ in
 {
   options.modules.home.nushell = {
     enable = mkBoolOpt false;
+    plugins = mkOpt (listOf types.package) [ ];
   };
 
   imports = [ ./config-symlinks.nix ];
 
   config = mkIf cfg.enable {
     modules.home.configSymlinks.enable = true;
+
+    modules.home.nushell.plugins = with pkgs.nushellPlugins; [
+      # Git status plugin for Nushell
+      gstat
+      # Formats plugin for Nushell
+      formats
+      # Nushell plugin to query JSON, XML, and various web data
+      query
+      # Nushell plugin for easily converting between common units
+      units
+      # Nushell plugin to list system network interfaces
+      net
+    ];
+
     home.packages = with pkgs; [
       # Modern shell written in Rust
       nushell
-    ];
+      # Nushell formatter
+      nufmt
+    ] ++ cfg.plugins;
 
     modules.home.configSymlinks.entries =
       map (name: "nushell/${name}") [
