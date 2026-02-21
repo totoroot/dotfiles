55b4443daf0665d6c6c74513a8212a13dd5b12e9
Matthias Thym <git@thym.at>
2026-02-21 13:08:25 +0100

    chore(nixos): reuse hosts/ips.nix for networking.hosts

--- hosts/nixos/personal.nix
+++ hosts/nixos/personal.nix
@@ -4,42 +4,7 @@ with lib;
 {
   networking.hosts =
     let
-      hostConfig = {
-        # Public IP Main VPS
-        "45.83.104.124" = [ "jam" ];
-
-        # Wifi Router
-        "10.0.0.1" = [ "white" ];
-        # Desktop PC
-        "10.0.0.2" = [ "purple" ];
-        # Home Server
-        "10.0.0.3" = [ "violet" ];
-        # Private Notebook
-        "10.0.0.4" = [ "grape" ];
-        # Smartphone
-        "10.0.0.5" = [ "fp5" ];
-        # Mac mini
-        "10.0.0.6" = [ "wine" ];
-
-        "192.168.0.20" = [ "black" ];
-        "193.30.121.17" = [ "phlox" ];
-
-        "100.64.0.1" = [ "jam-ts" ];
-        "100.64.0.2" = [ "purple-ts" ];
-        "100.64.0.3" = [ "violet-ts" ];
-        "100.64.0.4" = [ "grape-ts" ];
-        "100.64.0.5" = [ "fp5-ts" ];
-        # "100.64.0.6" = [ "wine-ts" ];
-
-        # "192.168.0.4" = [ "printer" ];
-        # "192.168.8.103" = [ "Lakka" ];
-        # "192.168.8.105" = [ "steamdeck" ];
-        # "192.168.8.106" = [ "raspberry" ];
-        # "192.168.8.107" = [ "grape" ];
-        # "192.168.8.108" = [ "sangria" ];
-        # "192.168.8.109" = [ "mulberry" ];
-        # "192.168.8.254" = [ "moooh" ];
-      };
+      hostConfig = import ../../hosts/ips.nix;
       hosts = flatten (attrValues hostConfig);
       hostName = config.networking.hostName;
     in
