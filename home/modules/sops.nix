e7376a3ccef4af7d7540c55c16dd48ba394ed731
Matthias Thym <git@thym.at>
2026-02-22 16:15:19 +0100

    feat(sops): add zsh alias wrapper with host key path feat(sops): install sops and sops-age via home module

---
+++ home/modules/sops.nix
@@ -0,0 +1,23 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let
+  cfg = config.modules.home.sops;
+  hostName = lib.attrsets.attrByPath [ "networking" "hostName" ] null config;
+  keyFile =
+    if hostName != null then "/var/lib/sops-nix/${hostName}.txt" else "/var/lib/sops-nix/$(hostname -s).txt";
+in
+{
+  options.modules.home.sops = {
+    enable = mkBoolOpt true;
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = with pkgs; [ sops sops-age ];
+    modules.home.zsh.rcInit = ''
+      sops() { SOPS_AGE_KEY_FILE="${keyFile}" TERM=xterm SOPS_EDITOR="micro" command sops "$@"; }
+      sops-edit() { SOPS_AGE_KEY_FILE="${keyFile}" TERM=xterm SOPS_EDITOR="micro" command sops "$@"; }
+    '';
+  };
+}
