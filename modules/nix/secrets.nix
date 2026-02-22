6eb23754efa44ff4b4341fb97e3135f4f29d6811
Matthias Thym <git@thym.at>
2026-02-22 17:23:17 +0100

    fix(sops): enable systemd activation for secrets on all hosts

--- modules/nix/secrets.nix
+++ modules/nix/secrets.nix
@@ -24,6 +24,7 @@ in
   config = mkIf cfg.enable {
     sops = {
       age.keyFile = cfg.ageKeyFile;
+      useSystemdActivation = true;
     } // (mkIf (cfg.defaultFile != null) {
       defaultSopsFile = cfg.defaultFile;
     });
