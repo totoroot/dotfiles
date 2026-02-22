a29001b5720555abcdb71239c3c553532707010c
Matthias Thym <git@thym.at>
2026-02-22 02:21:28 +0100

    revert(sddm): drop custom theme patching and use stock sddm
    - plasma.nix now just package = pkgs.kdePackages.sddm;
    - LXQt/XFCE no longer add extra packages or theme patching.

--- modules/desktop/environments/xfce.nix
+++ modules/desktop/environments/xfce.nix
@@ -11,39 +11,8 @@ in {
   config = mkIf cfg.enable {
     services = {
       # Display Manager for XFCE
-      displayManager.sddm = let
-        sddmBase = pkgs.kdePackages.sddm;
-        sddmWithGreeter = pkgs.runCommand "sddm-with-greeter" { } ''
-          cp -r ${sddmBase} $out
-          chmod -R u+w $out
-          if [ ! -x "$out/bin/sddm-greeter" ]; then
-            for candidate in "$out/libexec/sddm-greeter" "$out/lib/sddm/sddm-greeter"; do
-              if [ -x "$candidate" ]; then
-                ln -s "$candidate" "$out/bin/sddm-greeter"
-                break
-              fi
-            done
-          fi
-        '';
-      in {
+      displayManager.sddm = {
         enable = true;
-        extraPackages = let
-          greeterBin = "${sddmWithGreeter}/bin/sddm-greeter";
-          sddmDraculaRaw =
-            if pkgs ? sddm-theme-dracula then pkgs.sddm-theme-dracula
-            else if pkgs ? sddmThemes && pkgs.sddmThemes ? dracula then pkgs.sddmThemes.dracula
-            else null;
-          sddmDracula =
-            if sddmDraculaRaw != null then
-              pkgs.runCommand "sddm-theme-dracula-patched" { } ''
-                cp -r ${sddmDraculaRaw} $out
-                chmod -R u+w $out
-                find $out -type f -exec \
-                  sed -i "s|/nix/store/.*/bin/sddm-greeter|${greeterBin}|g" {} \;
-              ''
-            else null;
-        in
-        lib.optional (sddmDracula != null) sddmDracula;
       };
       xserver = {
         enable = true;
