9b2e4e4f93bc299fc713a2d49d7dd11e2629bee5
Matthias Thym <git@thym.at>
2026-03-08 14:00:18 +0100

    chore(purple): home.file

--- hosts/nixos/purple/default.nix
+++ hosts/nixos/purple/default.nix
@@ -259,23 +259,6 @@
   # To be able to use ttyUSB etc.
   user.extraGroups = [ "dialout" ];
 
-
-  home.file = {
-    "Desktop/.use".text = "desktop";
-    "Development/.use".text = "development";
-    "Documents/.use".text = "documents";
-    "Downloads/.use".text = "downloads";
-    "Games/.use".text = "games";
-    # "Music/.use".text = "music";
-    "Pictures/.use".text = "photos and graphics";
-    "Public/.use".text = "shared files";
-    "Resources/.use".text = "resources";
-    "Sync/.use".text = "synchronised files";
-    "Sync/notes.use".text = "notes";
-    "Temp/.use".text = "temporary files";
-    "Videos/.use".text = "videos";
-  };
-
   home-manager.users.${config.user.name} = { config, ... }: {
     imports = [
       ../../../home/bridge.nix
@@ -302,6 +285,19 @@
     };
 
     home.file = {
+      "Desktop/.use".text = "desktop";
+      "Development/.use".text = "development";
+      "Documents/.use".text = "documents";
+      "Downloads/.use".text = "downloads";
+      "Games/.use".text = "games";
+      # "Music/.use".text = "music";
+      "Pictures/.use".text = "photos and graphics";
+      "Public/.use".text = "shared files";
+      "Resources/.use".text = "resources";
+      "Sync/.use".text = "synchronised files";
+      "Sync/notes.use".text = "notes";
+      "Temp/.use".text = "temporary files";
+      "Videos/.use".text = "videos";
       "Notes/".source = config.lib.file.mkOutOfStoreSymlink "/home/mathym/Sync/notes/";
       "Trash/".source = config.lib.file.mkOutOfStoreSymlink "/home/mathym/.local/share/Trash/files/";
     };
