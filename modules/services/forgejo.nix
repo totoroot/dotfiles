e5909e0b37aae874101ccf557bef4e193352e50f
Matthias Thym <git@thym.at>
2026-03-29 20:33:33 +0200

    refactor(forgejo): migrate module from services.gitea to native services.forgejo options

--- modules/services/forgejo.nix
+++ modules/services/forgejo.nix
@@ -13,42 +13,40 @@ in
   };
 
   config = mkIf cfg.enable {
-    # I prefer git@... ssh addresses over gitea@...
+    # Keep the familiar git SSH identity while running Forgejo.
     users.users.git = {
       isSystemUser = true;
       useDefaultShell = true;
-      home = "/var/lib/gitea";
-      group = "gitea";
+      home = "/var/lib/forgejo";
+      group = "forgejo";
     };
 
-    services.gitea = {
+    services.forgejo = {
       enable = true;
       package = pkgs.forgejo;
 
       user = "git";
-      createDatabase = false;
-      database.user = "forgejo";
-      database.type = "postgres";
-      database.name = "forgejo";
+      group = "forgejo";
 
-      disableRegistration = true;
+      database = {
+        type = "postgres";
+        name = "forgejo";
+        user = "forgejo";
+        createDatabase = false;
+      };
 
-      # We're assuming SSL-only connectivity
-      cookieSecure = true;
-      # Only log what's important
-      log.level = "Error";
       settings = {
         server = {
-          DISABLE_ROUTER_LOG = true;
           DOMAIN = hostName;
           ROOT_URL = "https://${hostName}";
           HTTP_ADDR = "127.0.0.1";
           HTTP_PORT = 3001;
-          SSH_DOMAIN = hostName;
+          DISABLE_SSH = false;
           SSH_PORT = 22;
-          START_SSH_SERVER = false;
         };
+        session.COOKIE_SECURE = true;
         service.DISABLE_REGISTRATION = true;
+        log.LEVEL = "Error";
       };
     };
 
@@ -62,6 +60,6 @@ in
       ];
     };
 
-    user.extraGroups = [ "gitea" ];
+    user.extraGroups = [ "forgejo" ];
   };
 }
