66331c53b2c9c29829ff08e0476f4fa78d7bca11
Matthias Thym <git@thym.at>
2026-03-29 23:39:13 +0200

    fix(ci): set required labels for codeberg gitea-actions-runner instance

--- modules/services/forgejo-runner.nix
+++ modules/services/forgejo-runner.nix
@@ -12,12 +12,15 @@ in
     name = mkOpt types.str "jam-codeberg-runner";
     url = mkOpt types.str "https://codeberg.org";
     tokenFile = mkOpt types.str "/var/secrets/forgejo-runner-codeberg.token";
+
+    labels = mkOpt (types.listOf types.str) [ "native:host" ];
   };
 
   config = mkIf cfg.enable {
     services.gitea-actions-runner.instances.codeberg = {
       enable = true;
       inherit (cfg) name url tokenFile;
+      labels = cfg.labels;
     };
   };
 }
