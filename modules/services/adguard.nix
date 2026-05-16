344040e9864864b86cddee377cf9467cc3975d74
Matthias Thym <git@thym.at>
2026-05-16 16:09:37 +0200

    adguard: change upstream DNS format to standard IP#domain format

--- modules/services/adguard.nix
+++ modules/services/adguard.nix
@@ -53,11 +53,12 @@ in
           ];
           # Use multiple upstream DNS providers for redundancy
           upstream_dns = [
-            "https://dns.quad9.net/dns-query"
-            "https://cloudflare-dns.com/dns-query"
-            "https://dns.google/dns-query"
-            "tls://1.1.1.1"
-            "tls://1.0.0.1"
+            "9.9.9.9#dns.quad9.net"
+            "149.112.112.112#dns.quad9.net"
+            "1.1.1.1#cloudflare-dns.com"
+            "1.0.0.1#cloudflare-dns.com"
+            "8.8.8.8#dns.google"
+            "8.8.4.4#dns.google"
           ];
           # Enable DNS caching
           caching = true;
