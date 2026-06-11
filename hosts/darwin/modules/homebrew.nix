ecfbd6d48ec0c9bd32a45a73d3660c1f6e230c76
Matthias Thym <git@thym.at>
2026-06-11 12:16:58 +0200

    fix(homebrew): stop forcing tap-based resolution

--- hosts/darwin/modules/homebrew.nix
+++ hosts/darwin/modules/homebrew.nix
@@ -7,16 +7,12 @@
     upgrade = true;
     cleanup = "zap";
     extraFlags = [ "--force" ];
-    extraEnv = {
-      HOMEBREW_NO_INSTALL_FROM_API = "1";
-    };
   };
   homebrew.taps = [];
 
   environment.variables = {
     HOMEBREW_NO_UPDATE_REPORT_FORMULAE = "TRUE";
     HOMEBREW_NO_UPDATE_REPORT_CASKS = "TRUE";
-    HOMEBREW_NO_INSTALL_FROM_API = "1";
   };
 
   environment.systemPath = lib.mkAfter [
